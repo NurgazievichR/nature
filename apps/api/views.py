8a3b981b817a8f68433412be211d15d385f28656
left-try <lerest.go@gmail.com>
2024-11-21 11:23:38 +0600

    [Feature] Provide API to generate QR

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,8 +1,8 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 
-from apps.app.models import Place
+from apps.app.models import Place, OneTimePass
 
 
 class PlacesAPIView(APIView):
@@ -21,4 +21,19 @@ class PlacesAPIView(APIView):
                 'location': place.location,
             }
             data.append(place_data)
+        return JsonResponse(data, safe=False)
+
+class GenerateQRCodeAPIView(APIView):
+
+    @staticmethod
+    def get(request):
+        place = request.GET.get('place')
+        place_obj = get_object_or_404(Place, id=place.id)
+        one_time_pass = OneTimePass.objects.create(place=place_obj)
+        one_time_pass.code = one_time_pass.id
+        one_time_pass.save()
+        data = {
+            'code': one_time_pass.code,
+            'place': place.name,
+        }
         return JsonResponse(data, safe=False)
