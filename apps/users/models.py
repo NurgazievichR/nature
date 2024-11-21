c43bd0b803d88d3c4d931c0fb8fb93f17154e02d
left-try <lerest.go@gmail.com>
2024-11-21 10:02:38 +0600

    [Feature] Add models for User, OneTime pass and subscription pass

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    role = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
